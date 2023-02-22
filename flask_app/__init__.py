cb0ae318b72faa55b649c842bb96826551a59999
Jorge Leon <84067777+jorge2751@users.noreply.github.com>
2023-02-22 10:21:59 -0800

    Fixes

--- flask_app/__init__.py
+++ flask_app/__init__.py
@@ -1,5 +1,5 @@
 from flask import Flask
 
 app = Flask(__name__)
-app.secret_key = 'super_secret_key'
+app.secret_key = 'secret'
 
--- flask_app/controllers/dojos.py
+++ flask_app/controllers/dojos.py
@@ -1,4 +1,4 @@
-from flask import render_template, request, redirect, url_for
+from flask import render_template, request, redirect
 from flask_app.models.dojo import Dojo
 from flask_app import app
 
